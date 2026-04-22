8545e2f535d297049dc21a3dff6f073223cd3ee4
RyanAella <72981983+RyanAella@users.noreply.github.com>
2026-04-22 08:29:37 +0000

    Deploying to gh-pages from @ RyanAella/dialogue_lab@4654c86fade1bbe96e5f33536d8d1ccd9389be20 🚀

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  PROXY_URL: "https://kite2.site/chat.php",
+  PROXY_URL: "https://kite2.site/dialogue_lab/chat.php",
   MODEL: "gpt-4o",
   CHAT_TEMPERATURE: 0.7,
   MENTOR_TEMPERATURE: 0.3,
